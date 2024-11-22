dc2557c18cb826001972a8a3920e82f613a1b656
Lucas Roveri <lucas.vilela2019@outlook.com>
2024-11-22 08:50:59 -0300

    feat: garantindo que o processo não esteja rodando ao compilar e passando nome por argumento

--- compile.py
+++ compile.py
@@ -1,10 +1,16 @@
 import os
+import sys
+import subprocess
 
+name = sys.argv[1]
 files = []
 for file in os.listdir('./src'):
     if file.endswith('cpp'):
         files.append('src/' +file);
 
+if name+'.exe' in subprocess.check_output(['tasklist'], shell=True, universal_newlines=True):
+    subprocess.run(['taskkill', '/IM', name+'.exe', '/F'], check=True)
+
 
 output = ' '.join(files)    
-os.system('g++ ' + output + ' -o texteditor' )
+os.system('g++ ' + output + ' -o' + name )
